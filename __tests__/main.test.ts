9f8015dd0c9829ecc98675b86c835dae918e1a78
yukidaruma <yukidaruma@users.noreply.github.com>
2020-10-04 00:59:17 +0900

    `prettier --write .`
    Also make prettier format YAML and Markdown.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -18,10 +18,7 @@ describe('greeter function', () => {
   // Assert if setTimeout was called properly
   it('delays the greeting by 2 seconds', () => {
     expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), Delays.Long);
   });
 
   // Assert greeter result
